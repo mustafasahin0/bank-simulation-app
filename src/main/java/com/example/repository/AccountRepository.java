90fcb26d241f96ccf445b7e430f24b23de6c7efe
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-10-01 15:30:55 -0600

    added Transaction service

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,10 +1,12 @@
 package com.example.repository;
 
+import com.example.exception.RecordNotFoundException;
 import com.example.model.Account;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Component
 public class AccountRepository {
@@ -18,5 +20,9 @@ public class AccountRepository {
 
     public List<Account> findAll() {
         return accountList;
+    }
+
+    public void findById(UUID id) {
+        accountList.stream().filter(each -> each.getId().equals(id)).findFirst().orElseThrow(() -> new RecordNotFoundException("Account not found in DB"));
     }
 }
--- src/main/java/com/example/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/example/service/impl/TransactionServiceImpl.java
@@ -1,16 +1,25 @@
 package com.example.service.impl;
 
+import com.example.exception.BadRequestException;
 import com.example.model.Account;
 import com.example.model.Transaction;
+import com.example.repository.AccountRepository;
 import com.example.service.TransactionService;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Component
 public class TransactionServiceImpl implements TransactionService {
+
+    private final AccountRepository accountRepository;
+
+    public TransactionServiceImpl(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
 
     @Override
     public Transaction makeTransfer(Account sender, Account receiver, BigDecimal amount, Date creatationDate, String message) {
@@ -34,6 +43,21 @@ public class TransactionServiceImpl implements TransactionService {
             - Does account ids are same?
             - Does account exists in the DB?
          */
+        if(sender == null || receiver == null) {
+            throw new BadRequestException("Sender or receiver cannot be null");
+        }
+
+        // if accounts are the same throw BadRequestException "Accounts needs to be different"
+        if(sender.getId().equals(receiver.getId())) {
+            throw new BadRequestException("Accounts needs to be different");
+        }
+
+        findAccountById(sender.getId());
+        findAccountById(receiver.getId());
+    }
+
+    private void findAccountById(UUID id) {
+        accountRepository.findById(id);
     }
 
     @Override
