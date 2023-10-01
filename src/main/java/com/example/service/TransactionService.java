b9c29d2ea9f315cf2f3dc421be1d4d49df9f9e75
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-10-01 13:10:09 -0600

    added TransactionService

---
+++ src/main/java/com/example/service/TransactionService.java
@@ -0,0 +1,15 @@
+package com.example.service;
+
+import com.example.model.Account;
+import com.example.model.Transaction;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+public interface TransactionService {
+
+    Transaction makeTransfer(Account sender, Account receiver, BigDecimal amount, Date creatationDate, String message);
+
+    List<Transaction> findAllTransactions();
+}
