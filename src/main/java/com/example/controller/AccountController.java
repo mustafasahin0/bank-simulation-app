e2c253b844036bb1d3dba3628537d4b7f87142ed
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-10-02 23:11:17 -0600

    added AccountController

--- src/main/java/com/example/controller/AccountController.java
+++ src/main/java/com/example/controller/AccountController.java
@@ -1,4 +1,21 @@
 package com.example.controller;
 
+import com.example.service.AccountService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AccountController {
+
+    AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/index")
+    public String index() {
+        return "/account/index";
+    }
 }
