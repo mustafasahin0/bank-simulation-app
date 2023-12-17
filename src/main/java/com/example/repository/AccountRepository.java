3854d757cb32c59a2ecf672daa27ef38c2349c09
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-12-16 18:50:24 -0700

    Repositories are updated

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,32 +1,35 @@
 package com.example.repository;
 
 import com.example.dto.AccountDTO;
+import com.example.entity.Account;
 import com.example.exception.RecordNotFoundException;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
-public class AccountRepository {
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
 
-    public static List<AccountDTO> accountDTOList = new ArrayList<>();
-
-    public AccountDTO save(AccountDTO accountDTO){
-        accountDTOList.add(accountDTO);
-        return accountDTO;
-    }
-
-    public List<AccountDTO> findAll() {
-        return accountDTOList;
-    }
-
-    public AccountDTO findById(Long id) {
-        //TASK
-        //complete the method, that find the account inside the list, if not
-        //throw RecordNotFoundException
-        return accountDTOList.stream().filter(account -> account.getId().equals(id))
-                .findAny().orElseThrow(()-> new RecordNotFoundException("Account does not exist in the database."));
-
-    }
+//    public static List<AccountDTO> accountDTOList = new ArrayList<>();
+//
+//    public AccountDTO save(AccountDTO accountDTO){
+//        accountDTOList.add(accountDTO);
+//        return accountDTO;
+//    }
+//
+//    public List<AccountDTO> findAll() {
+//        return accountDTOList;
+//    }
+//
+//    public AccountDTO findById(Long id) {
+//        //TASK
+//        //complete the method, that find the account inside the list, if not
+//        //throw RecordNotFoundException
+//        return accountDTOList.stream().filter(account -> account.getId().equals(id))
+//                .findAny().orElseThrow(()-> new RecordNotFoundException("Account does not exist in the database."));
+//
+//    }
 }
--- src/main/java/com/example/repository/TransactionRepository.java
+++ src/main/java/com/example/repository/TransactionRepository.java
@@ -1,40 +1,43 @@
 package com.example.repository;
 
 import com.example.dto.TransactionDTO;
+import com.example.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Component
-public class TransactionRepository {
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    public static List<TransactionDTO> transactionDTOList = new ArrayList<>();
-
-    public TransactionDTO save(TransactionDTO transactionDTO){
-        transactionDTOList.add(transactionDTO);
-        return transactionDTO;
-    }
-
-    public List<TransactionDTO> findAll() {
-        return transactionDTOList;
-    }
-
-    public List<TransactionDTO> findLast10Transactions() {
-        //write a stream that sort the transactions based on creation date
-        //and only return 10 of them
-        return transactionDTOList.stream()
-                .sorted(Comparator.comparing(TransactionDTO::getCreateDate).reversed())
-                .limit(10)
-                .collect(Collectors.toList());
-    }
-
-    public List<TransactionDTO> findTransactionListByAccountId(Long id) {
-        //if account id is used either as a sender or receiver, return those transactions
-        return transactionDTOList.stream()
-                .filter(transactionDTO -> transactionDTO.getSender().equals(id) || transactionDTO.getReceiver().equals(id))
-                .collect(Collectors.toList());
-    }
+//    public static List<TransactionDTO> transactionDTOList = new ArrayList<>();
+//
+//    public TransactionDTO save(TransactionDTO transactionDTO){
+//        transactionDTOList.add(transactionDTO);
+//        return transactionDTO;
+//    }
+//
+//    public List<TransactionDTO> findAll() {
+//        return transactionDTOList;
+//    }
+//
+//    public List<TransactionDTO> findLast10Transactions() {
+//        //write a stream that sort the transactions based on creation date
+//        //and only return 10 of them
+//        return transactionDTOList.stream()
+//                .sorted(Comparator.comparing(TransactionDTO::getCreateDate).reversed())
+//                .limit(10)
+//                .collect(Collectors.toList());
+//    }
+//
+//    public List<TransactionDTO> findTransactionListByAccountId(Long id) {
+//        //if account id is used either as a sender or receiver, return those transactions
+//        return transactionDTOList.stream()
+//                .filter(transactionDTO -> transactionDTO.getSender().equals(id) || transactionDTO.getReceiver().equals(id))
+//                .collect(Collectors.toList());
+//    }
 }
