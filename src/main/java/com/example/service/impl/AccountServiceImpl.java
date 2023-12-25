8f017d21bcd6a23a774142433e8e1df5668d3b7a
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-12-25 10:31:14 +0300

    AccountMapper is created.

--- src/main/java/com/example/service/impl/AccountServiceImpl.java
+++ src/main/java/com/example/service/impl/AccountServiceImpl.java
@@ -2,8 +2,10 @@ package com.example.service.impl;
 
 
 import com.example.dto.AccountDTO;
+import com.example.entity.Account;
 import com.example.enums.AccountStatus;
 import com.example.enums.AccountType;
+import com.example.mapper.AccountMapper;
 import com.example.repository.AccountRepository;
 import com.example.service.AccountService;
 import org.springframework.stereotype.Component;
@@ -11,36 +13,39 @@ import org.springframework.stereotype.Component;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Component
 public class AccountServiceImpl implements AccountService {
 
     private final AccountRepository accountRepository;
+    private final AccountMapper accountMapper;
 
-
-    public AccountServiceImpl(AccountRepository accountRepository) {
+    public AccountServiceImpl(AccountRepository accountRepository, AccountMapper accountMapper) {
         this.accountRepository = accountRepository;
+        this.accountMapper = accountMapper;
     }
 
     @Override
     public AccountDTO createNewAccount(BigDecimal balance, Date createDate, AccountType accountType, Long userId) {
         //we need to create Account object
-        AccountDTO accountDTO = new AccountDTO();
+//        AccountDTO accountDTO = new AccountDTO();
         //save into the database(repository)
         //return the object created
-        return accountRepository.save(accountDTO);
+        return accountMapper.convertToDTO(accountRepository.save(new Account()));
     }
 
     @Override
     public List<AccountDTO> listAllAccount() {
-        return accountRepository.findAll();
+        return accountRepository.findAll().stream().map(accountMapper::convertToDTO).collect(Collectors.toList());
     }
 
     @Override
     public void deleteAccount(Long id) {
         //find the account belongs the id
-        AccountDTO accountDTO = accountRepository.findById(id);
+        AccountDTO accountDTO = accountMapper.convertToDTO(accountRepository.findById(id).get());
         //set status to deleted
         accountDTO.setAccountStatus(AccountStatus.DELETED);
     }
@@ -48,13 +53,13 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public void activateAccount(Long id) {
         //find the account belongs the id
-        AccountDTO accountDTO = accountRepository.findById(id);
+        AccountDTO accountDTO = accountMapper.convertToDTO(accountRepository.findById(id).get());
         //set status to active
         accountDTO.setAccountStatus(AccountStatus.ACTIVE);
     }
 
     @Override
     public AccountDTO retrieveById(Long id) {
-        return accountRepository.findById(id);
+        return accountRepository.findAllById(id);
     }
 }
