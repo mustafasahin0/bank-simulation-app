89b336b583170d319a805e17bf6b85a4f544c448
mustafasahin0 <sahinmustafaenes4@gmail.com>
2024-01-01 16:31:24 -0700

    Security

---
+++ src/main/java/com/example/entity/common/UserPrincipal.java
@@ -0,0 +1,51 @@
+package com.example.entity.common;
+
+import com.example.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserPrincipal implements UserDetails {
+
+    private final User user; // I want to access User properties.
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
---
+++ src/main/java/com/example/service/impl/SecurityServiceImpl.java
@@ -0,0 +1,31 @@
+package com.example.service.impl;
+
+import com.example.entity.User;
+import com.example.entity.common.UserPrincipal;
+import com.example.repository.UserRepository;
+import com.example.service.SecurityService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class SecurityServiceImpl implements SecurityService {
+
+    private UserRepository userRepository;
+
+    public SecurityServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUserName(username);
+
+        if(user == null) {
+            throw new UsernameNotFoundException("This user does not exists.");
+        }
+
+        return new UserPrincipal(user);
+
+
+    }
+}
